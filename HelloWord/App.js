e2a62aa739577ff2838ffbe6a98a59a675f9b4fb
encepsuryana <encep.suryanajr@gmail.com>
2021-01-10 13:06:27 +0700

    update 10/01/21

--- HelloWord/App.js
+++ HelloWord/App.js
@@ -1,59 +1,32 @@
-import React, { Component } from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React from 'react';
+import { View, Text, Image, ScrollView, TextInput } from 'react-native';
 
-// function App(props) {
-//   return (
-//     <View style={styles.container}>
-//       <Text style={styles.textRed}>Hello World!</Text>
-//     </View>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     justifyContent: 'center',
-//     alignItems:'center'
-//   },
-//   textRed: {
-//     color: 'blue'
-//   }
-// });
-
-export default class Example extends Component {
-  render() {
-    return (
-      <View style={{flex: 1, flexDirection:'row', flexWrap: 'wrap'}}>
-        <View style={styles.boxStyle}></View>
-        <View style={styles.boxStyle}></View>
-        <View style={styles.boxStyle}></View>
-        <View style={styles.boxStyle}></View>
-        <View style={styles.boxStyle}></View>
-        <View style={styles.boxStyle}></View>
-        <View style={styles.boxStyle}></View>
-        <View style={styles.boxStyle}></View>
-        <View style={styles.boxStyle}></View>
-        <View style={styles.boxStyle}></View>
-        <View style={styles.boxStyle}></View>
-        <View style={styles.boxStyle}></View>
-        <View style={styles.boxStyle}></View>
-        <View style={styles.boxStyle}></View>
-        <View style={styles.boxStyle}></View>
-        <View style={styles.boxStyle}></View>
+const App = () => {
+  return (
+    <ScrollView style={{alignContent: 'center'}}>
+      <Text>Some text</Text>
+      <View>
+        <Text>Some more text</Text>
+        <Image
+          source={{
+            uri: 'https://reactnative.dev/docs/assets/p_cat2.png',
+          }}
+          style={{ width: 200, height: 200 }}
+        />
       </View>
-    )
-  }
+      <TextInput
+        style={{
+          height: 50,
+          borderColor: 'gray',
+          borderWidth: 1,
+          borderRadius: 20,
+          paddingLeft: 20,
+          paddingRight: 20
+        }}
+        defaultValue="You can type in me"
+      />
+    </ScrollView>
+  );
 }
 
-const styles = StyleSheet.create({
-  boxStyle: {
-    height: 100,
-    width: 50,
-    borderWidth: 1,
-    backgroundColor: 'orange',
-    marginBottom: 5
-  },
-});
-
-
-// export default App;
+export default App;
